ae3b14ef5d3f997e50fd4460ee68298502dacc3d
Fede Camara Halac <camarafede@gmail.com>
2018-06-28 15:54:27 +0200

    makingscore

--- makescore.sh
+++ makescore.sh
@@ -4,19 +4,15 @@ INPUTDIR=$1
 
 cd ./output/$INPUTDIR	
 
-OUTPUT=$1-score.pdf
-
 instruments="BASS_FLUTE TRUMPET VIOLA"
 
 
 
 for i in $instruments
 do
+	OUTPUTFILE="$INPUTDIR-$i.pdf"
 	for j in {0..19}
 	do
-		# gs -sDEVICE=pdfwrite -sOutputFile="$OUTPUT" -dNOPAUSE -dEPSCrop -c "<</Orientation 2>> setpagedevice" -sPAPERSIZE=a3  -f $i-$j.ps -c quit
-		
-		# ls $i-$j.ps
-		# echo "$instrument"
+		 gs -sDEVICE=pdfwrite -sOutputFile="$OUTPUTFILE" -dNOPAUSE -dEPSCrop -c "<</Orientation 2>> setpagedevice" -sPAPERSIZE=a3  -f "$i-$j.ps" -c quit
 	done
 done
