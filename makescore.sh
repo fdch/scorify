5500708dfab9847fd09618be4ee8a75948028855
Utilisateur invité <Guest@mac-nomade-254.ircam.fr>
2018-06-28 15:32:22 +0200

    makescore

--- makescore.sh
+++ makescore.sh
@@ -2,18 +2,21 @@
 
 INPUTDIR=$1
 
-cd ../output/$INPUTDIR	
+cd ./output/$INPUTDIR	
 
 OUTPUT=$1-score.pdf
 
-instrument="BASS_FLUTE TRUMPET VIOLA"
+instruments="BASS_FLUTE TRUMPET VIOLA"
 
 
 
 for i in $instruments
 do
-	for j in $pages 
+	for j in {0..19}
 	do
-		gs -sDEVICE=pdfwrite -sOutputFile="$OUTPUT" -dNOPAUSE -dEPSCrop -c "<</Orientation 2>> setpagedevice" -sPAPERSIZE=a3  -f $i-$j.ps -c quit
+		# gs -sDEVICE=pdfwrite -sOutputFile="$OUTPUT" -dNOPAUSE -dEPSCrop -c "<</Orientation 2>> setpagedevice" -sPAPERSIZE=a3  -f $i-$j.ps -c quit
+		
+		# ls $i-$j.ps
+		# echo "$instrument"
 	done
 done
